9eb517d7a6546afe9b587e87cce859d221cf4d53
Fabio Fattori <fabio.fattori3@studio.unibo.it>
2024-10-31 10:31:47 +0100

    introduction to virtual enviroment

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-clear
-echo "starting AI for macOs / Linux"
-
-
-python ./index.py
--- test.py
+++
@@ -1,5 +0,0 @@
-import utilities.DataSetManager as dsm
-import classes.AmbrogioResNet50 as ar50
-
-model = ar50.AmbrogioNet50()
-#model.train()
